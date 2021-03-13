8c31ce8c2b9c0fc560c94e880f2cff39d0a8329f
Abhishek Mishra <abhishekmishra3@gmail.com>
2021-03-14 00:28:26 +0530

    added incomplete ball

--- piecer.h
+++ piecer.h
@@ -70,6 +70,7 @@ int main(int argc, char *argv[])
         draw(dt);
 
         showCanvas();
+        // wprintf(L"%ld\n", dt);
     }
 }
 
--- test0.c
+++ test0.c
@@ -1,12 +1,34 @@
 #include "piecer.h"
 
+size_t x = 0;
+size_t y = 0;
+
+//ball position
+double ballx = 0;
+double bally = 0;
+
+//ball velocity
+double balldx = 0.00001;
+double balldy = 0.00001;
+
 void setup()
 {
 	// setup() is run once at startup
 }
 
-size_t x = 0;
-size_t y = 0;
+void ball_update(long dt)
+{
+	ballx = ballx + (balldx * dt);
+	bally = bally + (balldy * dt);
+	if (ballx > width || ballx < 0)
+	{
+		balldx = balldx * -1.0;
+	}
+	if (bally > height || bally < 0)
+	{
+		balldy = balldy * -1.0;
+	}
+}
 
 void draw(long dt)
 {
@@ -15,30 +37,33 @@ void draw(long dt)
 	//set background character
 	background(L' ');
 
-	// set point 0,0 to A
-	point(0, 0, L'A');
+	point(ballx, bally, L'X');
+	ball_update(dt);
 
-	// set point 3,4 to B
-	point(3, 4, L'B');
+	// // set point 0,0 to A
+	// point(0, 0, L'A');
 
-	// set point x, y to C
-	point(x, y, L'C');
+	// // set point 3,4 to B
+	// point(3, 4, L'B');
 
-	if (y == height)
-	{
-		y = 0;
-	}
-	else
-	{
-		y += 1;
-	}
+	// // set point x, y to C
+	// point(x, y, L'C');
 
-	// a couple of lines
-	line(20, 20, 20, 25);
-	line(10, 10, 20, 20);
+	// if (y == height)
+	// {
+	// 	y = 0;
+	// }
+	// else
+	// {
+	// 	y += 1;
+	// }
 
-	// a rectangle
-	rect(5, 5, 10, 10);
+	// // a couple of lines
+	// line(20, 20, 20, 25);
+	// line(10, 10, 20, 20);
 
-	//noLoop(); // noLoop stops the draw loop.
+	// // a rectangle
+	// rect(5, 5, 10, 10);
+
+	// noLoop(); // noLoop stops the draw loop.
 }
