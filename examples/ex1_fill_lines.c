909009a613990de253f36a59eb941e2fdf440d8d
Abhishek Mishra <abhishekmishra3@gmail.com>
2025-01-15 08:16:01 +0530

    added docs

--- examples/ex1_fill_lines.c
+++ examples/ex1_fill_lines.c
@@ -1,3 +1,46 @@
+/*
+Copyright 2021-25 Abhishek Mishra <neolateral.in>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the “Software”), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+/**
+ * @file ex1_fill_lines.c
+ * @author Abhishek Mishra
+ * @brief Example 1: Fill lines on the canvas
+ * @version 0.1
+ * @date 2025-01-15
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ * This example demonstrates a simple animation
+ * on the canvas. We slowly fill the canvas with lines
+ * starting from the top and moving downwards.
+ * 
+ * Each new line is added after a certain time interval.
+ * This interval is controlled by the `step_time` variable.
+ * 
+ * Once all lines are drawn, the canvas is cleared and the
+ * process starts again.
+ * 
+ * To exit the program, press `Ctrl+C`.
+ */
+
 #include "piecer.h"
 
 uinteger total_time = 0;
@@ -7,8 +50,15 @@ uinteger lines_to_draw = 0;
 
 void setup()
 {
+    // does nothing but is required
 }
 
+/**
+ * @brief Increment the number of lines to draw
+ * everty `step_time` microseconds.
+ * 
+ * @param dt delta time in microseconds
+ */
 void draw(uinteger dt)
 {
     // determine the number of lines to draw
