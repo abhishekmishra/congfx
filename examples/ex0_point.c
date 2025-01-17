b028c297f7bb9370cfba128117d158c69b36bbf2
Abhishek Mishra <abhishekmishra3@gmail.com>
2025-01-17 17:29:07 +0530

    changed background and stroke colours

--- examples/ex0_point.c
+++ examples/ex0_point.c
@@ -8,8 +8,8 @@ void setup()
 
 void draw(cg_uint dt)
 {
-    background(11);
-    stroke(14);
+    background(12);
+    stroke(13);
 
     // draw a point at the center of the canvas
     point(width / 2, height / 2, L'X');
