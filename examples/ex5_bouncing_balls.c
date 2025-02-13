685add3b8d2c60a593ad69e33fdfd5e2acdd90e4
Abhishek Mishra <abhishekmishra3@gmail.com>
2025-02-13 20:24:20 +0530

    some code organization

--- examples/ex5_bouncing_balls.c
+++ examples/ex5_bouncing_balls.c
@@ -21,9 +21,12 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #include "congfx.h"
 
+#define NUM_BALLS 10
+#define FPS_STR_LEN 100
+
 cg_uint x = 0;
 cg_uint y = 0;
-cg_char fps_str[100];
+cg_char fps_str[FPS_STR_LEN];
 
 typedef struct
 {
@@ -31,6 +34,90 @@ typedef struct
 	vec2 velocity;
 	cg_number colour;
 } ball;
+
+ball *make_ball();
+void dispose_ball(ball *b);
+void ball_update(ball *b, cg_uint dt);
+void ball_show(ball *b);
+cg_string calc_fps(cg_uint dt);
+
+int main(int argc, char *argv[])
+{
+	ball *_b = NULL;
+	ball **balls;
+
+	// create the graphics engine
+	int err = cg_create_graphics_fullscreen();
+	if (err != 0)
+	{
+		return err;
+	}
+
+	// initialize the balls
+	balls = (ball **)calloc(NUM_BALLS, sizeof(ball *));
+	if (balls == NULL)
+	{
+		wprintf(L"Unable to allocate balls array.\n");
+		exit(-1);
+	}
+	
+	for (cg_int i = 0; i < NUM_BALLS; i++)
+	{
+		_b = make_ball();
+		int x = cg_rand_int(0, width);
+		int y = cg_rand_int(0, height);
+		// wprintf(L"%d, %d\n", x, y);
+		_b->position = cg_make_vec2(x, y);
+		_b->velocity = cg_make_vec2(0.00001 * cg_rand_int(-10, 10), 0.00001 * cg_rand_int(-10, 10));
+		balls[i] = _b;
+	}
+	// for (cg_int i = 0; i < NUM_BALLS; i++)
+	// {
+	// 	wprintf(L"ball %lu at %Lf, %Lf\n", i, balls[i]->position[0], balls[i]->position[1]);
+	// }
+	// setup() is run once at startup
+	cg_frame_rate(60);
+	// cg_no_loop();
+
+	while (1)
+	{
+		// begin the draw
+		err = cg_begin_draw();
+		if (err != 0)
+		{
+			break;
+		}
+
+		// clear canvas before every frame
+		cg_clear_canvas();
+
+		// print title
+		cg_text(L"CONGFX: EXAMPLE#5: BOUNCING BALLS ", width / 2 - 17, 0);
+
+		// show and update all the balls
+		for (cg_int i = 0; i < NUM_BALLS; i++)
+		{
+			ball *a = balls[i];
+			ball_show(a);
+			ball_update(a, cg_get_deltatime_micros());
+		}
+
+		// show fps
+		cg_string fps_str = calc_fps(cg_get_deltatime_micros());
+		cg_text(fps_str, 0, 0);
+
+		// end the draw
+		cg_end_draw();
+	}
+
+	// destroy the graphics engine
+	cg_destroy_graphics();
+}
+
+void key_pressed(char c)
+{
+	// do nothing
+}
 
 ball *make_ball()
 {
@@ -51,9 +138,6 @@ void dispose_ball(ball *b)
 		free(b);
 	}
 }
-
-cg_int NUM_BALLS = 10;
-ball **balls;
 
 void ball_update(ball *b, cg_uint dt)
 {
@@ -91,73 +175,6 @@ cg_string calc_fps(cg_uint dt)
 {
 	cg_uint dt_millis = dt / 1000;
 	cg_uint fps = (1000.0 / dt_millis);
-	swprintf(fps_str, 100, L"FPS: %lu", fps);
+	swprintf(fps_str, FPS_STR_LEN, L"FPS: %lu", fps);
 	return fps_str;
-}
-
-int main(int argc, char *argv[])
-{
-	ball *_b = NULL;
-	balls = (ball **)calloc(NUM_BALLS, sizeof(ball *));
-	// create the graphics engine
-	int err = cg_create_graphics_fullscreen();
-	if (err != 0)
-	{
-		return err;
-	}
-
-	for (cg_int i = 0; i < NUM_BALLS; i++)
-	{
-		_b = make_ball();
-		int x = cg_rand_int(0, width);
-		int y = cg_rand_int(0, height);
-		// wprintf(L"%d, %d\n", x, y);
-		_b->position = cg_make_vec2(x, y);
-		_b->velocity = cg_make_vec2(0.00001 * cg_rand_int(-10, 10), 0.00001 * cg_rand_int(-10, 10));
-		balls[i] = _b;
-	}
-	// for (cg_int i = 0; i < NUM_BALLS; i++)
-	// {
-	// 	wprintf(L"ball %lu at %Lf, %Lf\n", i, balls[i]->position[0], balls[i]->position[1]);
-	// }
-	// setup() is run once at startup
-	cg_frame_rate(60);
-	// cg_no_loop();
-
-	while (1)
-	{
-		// begin the draw
-		err = cg_begin_draw();
-		if (err != 0)
-		{
-			break;
-		}
-
-		// clear canvas before every frame
-		cg_clear_canvas();
-
-		cg_text(L"CONGFX: EXPERIMENT IN C ", width / 2 - 12, height / 2);
-
-		for (cg_int i = 0; i < NUM_BALLS; i++)
-		{
-			ball *a = balls[i];
-			ball_show(a);
-			ball_update(a, cg_get_deltatime_micros());
-		}
-
-		// show fps
-		cg_string fps_str = calc_fps(cg_get_deltatime_micros());
-		cg_text(fps_str, 0, 0);
-
-		// end the draw
-		cg_end_draw();
-	}
-
-	// destroy the graphics engine
-	cg_destroy_graphics();
-}
-
-void key_pressed(char c)
-{
-	// do nothing
 }
