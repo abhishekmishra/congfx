6ebdd2ab0f95604f76b463b428ea29517aea0481
abhishekmishra <abhishekmishra@users.noreply.github.com>
2026-02-21 08:16:58 +0000

    deploy: bfc97458844b111e5ab59be3a9a2f522620f096f

--- api/html/search/defines_1.js
+++ api/html/search/defines_1.js
@@ -1,4 +1,6 @@
 var searchData=
 [
-  ['cg_5fget_5fdeltatime_0',['cg_get_deltatime',['../congfx_8h.html#a92c647e3e45b2ade6968c600079c644d',1,'congfx.h']]]
+  ['cg_5fget_5fdeltatime_0',['cg_get_deltatime',['../congfx_8h.html#a92c647e3e45b2ade6968c600079c644d',1,'congfx.h']]],
+  ['cg_5fplatform_5fposix_1',['CG_PLATFORM_POSIX',['../congfx_8h.html#a7f719d05b11aeeb95439916a9421aa43',1,'congfx.h']]],
+  ['cg_5fplatform_5fwindows_2',['CG_PLATFORM_WINDOWS',['../congfx_8h.html#aab3c2e26ef4acbd67a4dbc5883074799',1,'congfx.h']]]
 ];
