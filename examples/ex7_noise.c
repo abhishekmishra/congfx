0cdf590103849874fe49193ad38a20e97d5b4de3
Abhishek Mishra <abhishekmishra3@gmail.com>
2026-02-21 09:17:52 +0530

    now displaying letter with random colours

--- examples/ex7_noise.c
+++ examples/ex7_noise.c
@@ -1,10 +1,16 @@
+#include <stdlib.h>
 #define CONGFX_IMPLEMENTATION
 #include "congfx.h"
 
+// get random int between min and max inclusive
+int rand_between(int min, int max)
+{
+    return (rand() % (max - min + 1)) + min;
+}
+
 int main(int argc, char *argv[])
 {
-    cg_rgb_t fg_colour = {0, 0, 0};
-    cg_rgb_t bg_colour = {0, 100, 0};
+    cg_rgb_t bg_colour = {0, 0, 0};
 
     cg_frame_rate(60);
 
@@ -20,17 +26,20 @@ int main(int argc, char *argv[])
         // begin the draw
         cg_begin_draw();
 
+        // clear the background
         cg_background(bg_colour);
-        cg_stroke(fg_colour);
 
-        for (int i = 0; i < width - 1; i++)
+        for (int i = 0; i < width; i++)
         {
-            for (int j = 0; j < height - 1; j++)
+            for (int j = 0; j < height; j++)
             {
-                cg_stroke((cg_rgb_t){i, j*10, 255});
+                cg_stroke((cg_rgb_t){rand_between(0, 255), rand_between(0, 255), rand_between(0, 255)});
                 cg_point(i, j, L'X');
             }
         }
+
+        // reset to white
+        cg_stroke((cg_rgb_t){255, 255, 255});
 
         // print press escape to exit
         cg_text(L"Press ESC to exit", 0, height - 1);
